9e753db31a2885bace8702f94f5a4070d2a92afd
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-11-04 20:51:50 +0530

    todoItem and todoForm added

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import { TodoProvider } from './contexts';
+import { useEffect } from 'react';
 
 function App() {
   const {todos, setTodos} = useState([])
@@ -17,10 +18,29 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-  const toggleComplete = (id) => {}
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+
+
+  useEffect(() => {
+  
+    const todos = JSON.parse(  localStorage.getItem("todos"))
+    
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+
 
   return (
-    <TodoProvider value={todos, addTodo, updateTodo, deleteTodo, toggleComplete}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
 <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
